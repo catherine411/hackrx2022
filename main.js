43cf7f71b79d3feeb37a768bbba1446ef86cead6
Kim Hoang <kimberley.hoang528@gmail.com>
2022-07-17 11:24:03 -0400

    fixed home page

--- main.js
+++
@@ -1,6 +0,0 @@
-function chooseAdmin(){
-    document.getElementById("dropdownMenuButton1").innerHTML = 'Admin';
-}
-function choosePatient(){
-    document.getElementById("dropdownMenuButton1").innerHTML = 'Patient';
-}
